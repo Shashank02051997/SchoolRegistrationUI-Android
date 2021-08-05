45ec26e661350245ee696ef83c3138e516e13e66
Shashank Singhal <shashanksinghal02@gmail.com>
2021-08-05 15:40:18 +0530

    - Upgraded the dependencies

--- app/src/main/java/com/shashank/platform/schoolcollegeapp/Main2Activity.java
+++ app/src/main/java/com/shashank/platform/schoolcollegeapp/Main2Activity.java
@@ -1,11 +1,12 @@
 package com.shashank.platform.schoolcollegeapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -18,11 +19,6 @@ public class Main2Activity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main2);
         back = findViewById(R.id.back);
-        back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
+        back.setOnClickListener(view -> finish());
     }
 }
--- app/src/main/java/com/shashank/platform/schoolcollegeapp/MainActivity.java
+++ app/src/main/java/com/shashank/platform/schoolcollegeapp/MainActivity.java
@@ -1,13 +1,11 @@
 package com.shashank.platform.schoolcollegeapp;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,24 +15,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         join_us = findViewById(R.id.join_us);
         login = findViewById(R.id.login);
-        join_us.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),Main2Activity.class);
-                startActivity(intent);
-            }
+        join_us.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
+            startActivity(intent);
         });
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),Main3Activity.class);
-                startActivity(intent);
-            }
+        login.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), Main3Activity.class);
+            startActivity(intent);
         });
     }
 }
